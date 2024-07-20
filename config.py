624360533253bb6bb773c76cf92217718b809100
Bhavin Chavda <77386078+Bhavin-Chavda@users.noreply.github.com>
2024-07-20 13:29:26 +0530

    Update config.py
    Config update

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 class Config:
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'Bhavin@8818'
+    MYSQL_PASSWORD = 'password'
     MYSQL_DB = 'sde_test'
     MYSQL_PORT = 3306
